32f1e215122d0f67ac9d4e92fda59ae90d91e435
Alejandro P <apatino@seedem.co>
2021-02-16 09:37:07 -0500

    CSS seccion 4

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -2,11 +2,48 @@ import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import '../assets/styles/App.scss';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 
 const App = () => (
   <div className="App">
     <Header />
     <Search />
+
+    <Categories title="Tendencias">
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title="Originales">
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title="Populares">
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories>
+      <Carousel>
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Footer />
+
   </div>
 );
 
